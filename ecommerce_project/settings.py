0e7e2404eb551b197f1cb2d0fccaaed4d83beb9d
Waqar Ali <goraya.waqar@gmail.com>
2024-08-18 22:10:27 +0500

    static root
    root dir set for production

--- ecommerce_project/settings.py
+++ ecommerce_project/settings.py
@@ -203,7 +203,7 @@ cloudinary.config(
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
 MEDIA_URL = '/media/'
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
